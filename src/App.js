98738fb6325c3c7643dd771614905356431a8c19
Houssemeddine SALEM <salemhoussemeddine@gmail.com>
2021-07-22 09:04:52 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './style.css';
+import fotoTwo from './myimages/fotoTwo.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div-pos">
+       <div className="div-second-pos" style={{border:"solid 1px black", maxWidth:"100vw"}}>
+            <h1 className="title red mt-5 ">Houssemeddine SALEM</h1>
+            <br />
+            <img className='mb-5 shadow-lg p-3 mb-5 bg-body rounded' src="/fotoOne.jpg" alt="" width="320"/>
+            <br />
+            <img className='mb-5 shadow-lg p-3 mb-5 bg-body rounded' src={fotoTwo} alt="" width="320"/> 
+       </div> 
+            <iframe className="video-container mt-5" width="100%" height="220" src="https://www.youtube.com/embed/3zt_AelvldM?start=4"/>        
     </div>
   );
 }
